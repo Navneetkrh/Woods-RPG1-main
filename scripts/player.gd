0986c8a640798d3964fbb60f971894589993168a
Samaksh Verma <vsamaksH@gmail.com>
2023-09-25 20:18:51 +0530

    gg

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,33 @@
 extends CharacterBody2D
+class_name Player
 
+var enemy_in_attack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
 
-const SPEED = 80
+var SPEED = 80
+const walk=50
+const run=100
 var current_dir= "none"
 
 
 func _physics_process(delta):
 	player_movement(delta)
+	
+	enemy_attacks()
+	if(health<=0):
+		player_alive=false;
+		health=0;
+		print("player has been killed")
 
 func player_movement(delta):
+	
+	if Input.is_action_pressed("run"):
+		SPEED=run
+	else:
+		SPEED=walk
+		
 	
 	if Input.is_action_pressed("ui_right"):
 		play_anim(1)
@@ -75,3 +94,29 @@ func play_anim(movement):
 		elif movement == 0:
 			anim.play("idle")
 					
+
+
+func _on_hitbox_body_entered(body):
+	if body.has_method('enemy'):
+		enemy_in_attack_range = true;
+
+
+func _on_hitbox_body_exited(body):
+	if body.has_method('enemy'):
+		enemy_in_attack_range = false;
+
+
+func enemy_attacks():
+	if enemy_in_attack_range == true and enemy_attack_cooldown==true:
+		health -= 10;
+		enemy_attack_cooldown=false;
+		$Attack_Cool_Down.start()
+		print(health)
+
+	
+func player():
+	pass
+
+
+func _on_attack_cool_down_timeout():
+	enemy_attack_cooldown=true;	
